2c4417260b3f164dbc09211274282e282b3a1ae4
Felice Forby <feliceforby@gmail.com>
2018-06-28 21:45:41 +0000

    automatically backed up by learn

--- app/item.rb
+++ app/item.rb
@@ -1,19 +1,8 @@
 class Item
   attr_accessor :name, :price
-  
-  # @@all = []
 
   def initialize(name,price)
     @name = name
     @price = price
-    # @@all << self
   end
-  
-  # def self.all 
-  #   @@all
-  # end
-  
-  # def self.find_by_name(name)
-  #   self.all.find { |item| item.name == name }
-  # end
 end
